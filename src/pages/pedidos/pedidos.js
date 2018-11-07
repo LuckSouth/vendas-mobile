85bebe9e3de50c0863091da0795c046feec5b122
Fernando <fernandosma282@gmail.com>
2018-11-07 16:11:42 -0300

    v 0.0.10

--- src/pages/pedidos/pedidos.js
+++ src/pages/pedidos/pedidos.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity
+} from "react-native";
 import { estilo } from "../pedidos/pedidos.css";
 
 export default class Pedidos extends Component {
@@ -10,38 +16,57 @@ export default class Pedidos extends Component {
   render() {
     return (
       <View>
-        <View style={estilo.cardPedidos}>
-          <Text style={estilo.tituloCard}>Edvaldo Viera da Silva</Text>
-          <Text style={estilo.informacaoCentro}>R$ 450,26 </Text>
-          <View style={estilo.rodapeCard}>
-            <Text style={estilo.statusPedidos}>12/12/2012</Text>
-            <Text style={estilo.statusPedidos}>RECEBIDO</Text>
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate("PedidosInformacoes")}
+        >
+          <View style={estilo.cardPedidos}>
+            <Text style={estilo.tituloCard}>Edvaldo Viera da Silva</Text>
+            <Text style={estilo.informacaoCentro}>R$ 450,26 </Text>
+            <View style={estilo.rodapeCard}>
+              <Text style={estilo.statusPedidos}>12/12/2012</Text>
+              <Text style={estilo.statusPedidos}>RECEBIDO</Text>
+            </View>
           </View>
-        </View>
-        <View style={estilo.cardPedidos}>
-          <Text style={estilo.tituloCard}>Edvaldo Viera da Silva</Text>
-          <Text style={estilo.informacaoCentro}>R$ 450,26 </Text>
-          <View style={estilo.rodapeCard}>
-            <Text style={estilo.statusPedidos}>12/12/2012</Text>
-            <Text style={estilo.statusPedidos}>RECEBIDO</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate("PedidosInformacoes")}
+        >
+          <View style={estilo.cardPedidos}>
+            <Text style={estilo.tituloCard}>Bruno Vitor Carvalho</Text>
+            <Text style={estilo.informacaoCentro}>R$ 1.486,12 </Text>
+            <View style={estilo.rodapeCard}>
+              <Text style={estilo.statusPedidos}>28/10/2014</Text>
+              <Text style={estilo.statusPedidos}>RECEBIDO</Text>
+            </View>
           </View>
-        </View>
-        <View style={estilo.cardPedidos}>
-          <Text style={estilo.tituloCard}>Edvaldo Viera da Silva</Text>
-          <Text style={estilo.informacaoCentro}>R$ 450,26 </Text>
-          <View style={estilo.rodapeCard}>
-            <Text style={estilo.statusPedidos}>12/12/2012</Text>
-            <Text style={estilo.statusPedidos}>RECEBIDO</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate("PedidosInformacoes")}
+        >
+          <View style={estilo.cardPedidos}>
+            <Text style={estilo.tituloCard}>Noah Vitor Corte Real</Text>
+            <Text style={estilo.informacaoCentro}>R$ 47,12 </Text>
+            <View style={estilo.rodapeCard}>
+              <Text style={estilo.statusPedidos}>31/01/2015</Text>
+              <Text style={estilo.statusPedidos}>RECEBIDO</Text>
+            </View>
           </View>
-        </View>
-        <View style={estilo.cardPedidos}>
-          <Text style={estilo.tituloCard}>Edvaldo Viera da Silva</Text>
-          <Text style={estilo.informacaoCentro}>R$ 450,26 </Text>
-          <View style={estilo.rodapeCard}>
-            <Text style={estilo.statusPedidos}>12/12/2012</Text>
-            <Text style={estilo.statusPedidos}>RECEBIDO</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate("PedidosInformacoes")}
+        >
+          <View style={estilo.cardPedidos}>
+            <Text style={estilo.tituloCard}>Otávio Rodrigo Gonçalves</Text>
+            <Text style={estilo.informacaoCentro}>R$ 783,45 </Text>
+            <View style={estilo.rodapeCard}>
+              <Text style={estilo.statusPedidos}>01/05/2013</Text>
+              <Text style={estilo.statusPedidos}>RECEBIDO</Text>
+            </View>
           </View>
-        </View>
+        </TouchableOpacity>
       </View>
     );
   }
